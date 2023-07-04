ddb0d6273e954cf6945f155ac8ef76d8e23c214f
Dec <declan_meek@yahoo.com>
2023-07-04 12:33:22 +0100

    Pass all 7 checks of task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let greeting = '';
+    for(let i = 0; i < num; i++){
+        greeting += string
+    } if (num < 0){
+        return 'ERROR'
+    } else {
+    return greeting
+    }
 };
 
 // Do not edit below this line
