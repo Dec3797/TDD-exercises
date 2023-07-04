8292b63fe271feadff288605aff4d23cbfb22ce9
Dec <declan_meek@yahoo.com>
2023-07-04 14:39:40 +0100

    Pass all tests in exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let total = 0;
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)){
+        return 'ERROR'
+    } else {
+        if(min < max){
+        for (let i = min; i <= max; i++){
+            total += i;
+        }
+    } else if (min > max){
+        for (let i = min; i >= max; i--){
+            total += i;
+        }
+    } return total
+    }
 };
 
 // Do not edit below this line
