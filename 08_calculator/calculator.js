f4df27df0d845531a80cbe4a7cd3ba8b76a8d16a
Dec <declan_meek@yahoo.com>
2023-07-05 16:43:29 +0100

    Pass all tests in task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	total = 0;
+  if(array == 0){
+    return 0;
+  } else {
+  return array.reduce((array, total) => array + total)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((args, total) => args * total)
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number == 0){
+    return 1
+  } else {
+    return (number * factorial(number - 1))
+}
 };
 
 // Do not edit below this line
