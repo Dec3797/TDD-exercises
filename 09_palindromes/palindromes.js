33507d1e82c51908bebd89c939f1971ca0bf3060
Dec <declan_meek@yahoo.com>
2023-07-05 17:16:32 +0100

    Pass all tests of task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let split = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return split.split('').reverse().join('') == split;
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
